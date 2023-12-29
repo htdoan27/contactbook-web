718e7d33bc7c20e1a36f3e13eb0cd89566d66133
htdoan27 <huynhthiendoan27@gmail.com>
2023-12-29 17:11:56 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/config/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const config = {
+  app: {
+    port: process.env.PORT,
+  },
+};
+
+
+module.exports = config;
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create = (req, res) => {
+    res.send({ messgae: "create handler" });
+  };
+  exports.findAll = (req, res) => {
+    res.send({ messgae: "findAll handler" });
+  };
+  exports.findOne = (req, res) => {
+    res.send({ messgae: "findOne handler" });
+  };
+  exports.update = (req, res) => {
+    res.send({ messgae: "update handler" });
+  };
+  exports.delete = (req, res) => {
+    res.send({ messgae: "delete handler" });
+  };
+  exports.deleteAll = (req, res) => {
+    res.send({ messgae: "deleteAll handler" });
+  };
+  exports.findAllFavorite = (req, res) => {
+    res.send({ messgae: "findAllFavorite handler" });
+  };
